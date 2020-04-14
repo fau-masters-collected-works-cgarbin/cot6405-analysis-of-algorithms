21c3234c0806263ea4f818f56bc03df2fd470c79
Christian Garbin <garbin.christian@gmail.com>
2020-04-13 20:40:24 -0400

    Fix instructions

--- longest-common-subsequence/line_profiling.py
+++ longest-common-subsequence/line_profiling.py
@@ -5,13 +5,13 @@ Use this file to line-profile other pieces of code.
 - Install line_profiler
 - import line_profile in the module that has the function to profile
 - Add @profile to the function declaration
-- Run `kernprof --view -l profiling.py`
+- Run `kernprof --view -l line_profiling.py`
 
 See https://github.com/pyutils/line_profiler.
 '''
 import random
 import lcs_utils
-import lcs_dynamic_programming_v2
+import lcs_hirschberg_numpy
 import lcs_test
 
 # IMPORTANT
@@ -21,9 +21,9 @@ import lcs_test
 
 random.seed(42)
 
-dna = lcs_utils.random_dna_sequence(10_000)
-dna_strand = lcs_utils.random_dna_sequence(1_000)
+dna = lcs_utils.random_dna_sequence(1_000)
+dna_strand = lcs_utils.random_dna_sequence(100)
 
 # Change this line to call the function to bre profiled
 # Remember to import line_profiler in that file and add @profile
-lcs_dynamic_programming_v2.lcs(dna, dna_strand)
+lcs_hirschberg_numpy.lcs(dna, dna_strand)
