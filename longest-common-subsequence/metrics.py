05ff1ec68e8157735a2e7ec3039489ce3e47b476
Christian Garbin <garbin.christian@gmail.com>
2020-04-18 10:30:10 -0400

    Improve memory measurement code (not final)

--- longest-common-subsequence/metrics.py
+++ longest-common-subsequence/metrics.py
@@ -13,58 +13,99 @@ import lcs_recursive
 import lcs_test
 import lcs_utils
 import time
+import lcs_empty
+import gc
 
 
+# The algorithms to collect metrics for
 Algorithm = namedtuple('Algorithm', ['function', 'description'])
 algorithms = [
     Algorithm(lcs_brute_force.lcs, 'Brute force'),
-    Algorithm(lcs_hirschberg.lcs, 'Hirschberg'),
-    Algorithm(lcs_hirschberg_numpy.lcs, 'Hirschberg NumPy'),
-    Algorithm(lcs_dynamic_programming_dict.lcs, 'Dynamic programming dict'),
     Algorithm(lcs_dynamic_programming_matrix_numpy.lcs,
               'Dynamic programming NumPy'),
+    Algorithm(lcs_hirschberg_numpy.lcs, 'Hirschberg NumPy'),
 ]
 
-#tests = [(10_000, 1_000), (100_000, 10_000)]
-tests = [(10_000, 1_000), (100_000, 1_000)]
-#tests = [(10_000, 1_000)]
-
-for dna_size, dna_strand_size in tests:
-    dna = lcs_utils.random_dna_sequence(dna_size)
-    dna_strand = lcs_utils.random_dna_sequence(dna_strand_size)
-
-    print('\nTimes for DNA {:,}, DNA strand {:,}'.format(
-        len(dna), len(dna_strand)))
-    for alg in algorithms:
-        start = time.process_time()
-
-        alg.function(dna, dna_strand)
-        total_time = time.process_time() - start
-
-        print('  {:>30}: {:.3f}'.format(alg.description, total_time))
-
-# Measure memory and time again
-# Measuring in a separate loop to check the performance impact of
-# memory profiling
-for dna_size, dna_strand_size in tests:
-    dna = lcs_utils.random_dna_sequence(dna_size)
-    dna_strand = lcs_utils.random_dna_sequence(dna_strand_size)
-
-    print('\nMemory and time for DNA {:,}, DNA strand {:,}'.format(
-        len(dna), len(dna_strand)))
-    for alg in algorithms:
-        start = time.process_time()
-
-        memory = memory_usage((alg.function, (dna, dna_strand)))
-        total_time = time.process_time() - start
-
-        print('  {:>30}: {:.3f} {:.3f} {}'.format(
-            alg.description, max(memory), total_time, memory[:5]))
+# The tests to execute
+tests = [(1_000, 100), (10_000, 1_000),  (100_000, 1_000)]
 
 
-# Memory profiling
-# See the "API" section in https://pypi.org/project/memory-profiler/
-#   example of how to monitor mem usage for a function
-# https://stackoverflow.com/a/15682871/336802
-# https://stackoverflow.com/a/26195843/336802 seem easier and less intrusive
-# https://stackoverflow.com/a/44619638/336802 also a simple one
+def runtime(repeat=2, verbose=True):
+    '''Measures algorithms' runtime.'''
+    for dna_size, dna_strand_size in tests:
+        # Create the test strings only once to correcly compare algorithms
+        dna = lcs_utils.random_dna_sequence(dna_size)
+        dna_strand = lcs_utils.random_dna_sequence(dna_strand_size)
+
+        if verbose:
+            print('\nTimes for DNA {:,}, DNA strand {:,}'.format(
+                len(dna), len(dna_strand)))
+
+        for alg in algorithms:
+            for _ in range(repeat):
+                start = time.process_time()
+                lcs = alg.function(dna, dna_strand)
+                total_time = time.process_time() - start
+
+                if verbose:
+                    print('  {:>30}: {:.3f}'.format(
+                        alg.description, total_time))
+
+                # Make that the algorithm is working correctly
+                assert(lcs_utils.is_subsequence(dna, lcs))
+
+
+def memory(repeat=2, verbose=True):
+    '''Measures algorithms' memory usage.
+
+    Memory usage has to be done separately from runtime meaurement because
+    tracking memory usage affects runtime. It is more notieable in the cases
+    where the algorithm finishes quickly, e.g. fast algorithms or small input.
+    '''
+    # Run all algorithms once to load all we need in teh Python environemtn.
+    # creating a baseline for memory usage
+    for dna_size, dna_strand_size in tests:
+        dna = lcs_utils.random_dna_sequence(dna_size)
+        dna_strand = lcs_utils.random_dna_sequence(dna_strand_size)
+        for alg in algorithms:
+            print('Warming up - {} {},{}'.format(alg.description,
+                                                 dna_size, dna_strand_size))
+            alg.function(dna, dna_strand)
+
+    for dna_size, dna_strand_size in tests:
+        # Create the test strings only once to correcly compare algorithms
+        dna = lcs_utils.random_dna_sequence(dna_size)
+        dna_strand = lcs_utils.random_dna_sequence(dna_strand_size)
+
+        if verbose:
+            print('\nMemory and time for DNA {:,}, DNA strand {:,}'.format(
+                len(dna), len(dna_strand)))
+
+        for alg in algorithms:
+            for _ in range(repeat):
+                # Run garbage collection so we don't measure memory left over
+                # from other algorithm runs
+                gc.collect()
+
+                # Get the current memory usage of the Python environment
+                mem_baseline = min([min(memory_usage(
+                    (lcs_empty.lcs), interval=0.01)) for _ in range(3)])
+
+                start = time.process_time()
+                mem_usage = memory_usage((alg.function, (dna, dna_strand)),
+                                         interval=0.01)
+                total_time = time.process_time() - start
+
+                if verbose:
+                    print('  {:>30}: {:.6f} {:.6f} {:.3f} {}'.format(
+                        alg.description, max(mem_usage), mem_baseline,
+                        total_time, mem_usage[:5]))
+
+
+# Use this to measure the current memory consumption
+for _ in range(5):
+    mem_usage = memory_usage((lcs_empty.lcs), interval=0.01)
+    print(mem_usage)
+
+
+memory()
