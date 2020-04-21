a60575248d64f2cb1a2752aa62445b1544443c9f
Christian Garbin <garbin.christian@gmail.com>
2020-04-21 08:06:20 -0400

    Make the functions behave closer to the actual functions

--- longest-common-subsequence/lcs_empty.py
+++ longest-common-subsequence/lcs_empty.py
@@ -1,6 +1,14 @@
-def _lcs():
+'''Measure baseline memory usage.
+
+Mimicks the interfaces of the LCS algorithms, but has no code.
+'''
+
+
+def _lcs(dna, dna_strand):
+    '''Mimicks an internal function of an LCS algorithm.'''
     pass
 
 
-def lcs():
-    pass
+def lcs(dna, dna_strand):
+    '''Mimicks an LCS algorithm function.'''
+    _lcs(dna, dna_strand)
