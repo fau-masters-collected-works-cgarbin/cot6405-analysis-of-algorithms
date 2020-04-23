eae10fc61ab12f8f1c9217ae161cc159113dbbcc
Christian Garbin <garbin.christian@gmail.com>
2020-04-23 08:14:59 -0400

    First version of the notebook

--- longest-common-subsequence/metrics.py
+++ longest-common-subsequence/metrics.py
@@ -2,6 +2,7 @@
 '''
 from collections import namedtuple
 from memory_profiler import memory_usage
+import pandas as pd
 import random
 import lcs_brute_force
 import lcs_dynamic_programming_dict
@@ -26,13 +27,22 @@ algorithms = [
     Algorithm(lcs_hirschberg_numpy.lcs, 'Hirschberg NumPy'),
 ]
 
+# DataFrame columns
+DF_ALGORITHM = 'Algorithm'
+DF_SEQ_SIZE = 'Sequence size'
+DF_SUBSEQ_SIZE = 'Subsequence size'
+DF_TEST_NUMBER = 'Test number'
+DF_RUNTIME = 'Runtime (s)'
+DF_MEMORY = 'Memory (KiB)'
+
+
 # The tests to execute
 #tests = [(1_000, 100), (10_000, 1_000), (100_000, 10_000)]
 # tests = [(1_000, 100), (10_000, 1_000), (100_000, 1_000)]
 tests = [(1_000, 100), (10_000, 1_000)]
 
 
-def runtime(repeat=2, verbose=1):
+def _runtime(repeat=2, verbose=1):
     '''Measures algorithms' runtime.
 
     Keyword Arguments:
@@ -49,38 +59,37 @@ def runtime(repeat=2, verbose=1):
             - The time to complete the run in milliseconds
     '''
     results = []
+
+    # Run one test
+    def _run_test(alg, seq_size, subseq_size):
+        start = time.process_time()
+        lcs = alg.function(dna, dna_strand)
+        total_time = time.process_time() - start
+        results.append([alg.description, dna_size, dna_strand_size,
+                        i+1, total_time])
+        # Make that the algorithm is working correctly
+        assert(lcs_utils.is_subsequence(dna, lcs))
+        if verbose >= 2:
+            print('  {:>30}: {:.3f}'.format(alg.description, results[-1][4]))
+
+    # Run all tests
     for dna_size, dna_strand_size in tests:
         # Create the test strings only once to correcly compare algorithms
         dna = lcs_utils.random_dna_sequence(dna_size)
         dna_strand = lcs_utils.random_dna_sequence(dna_strand_size)
 
-        if verbose >= 1:
-            print('\nTimes for DNA {:,}, DNA strand {:,}'.format(
-                len(dna), len(dna_strand)))
-
         for alg in algorithms:
             if verbose == 1:
                 print('\n{}: '.format(alg.description), end='', flush=True)
             for i in range(repeat):
-                start = time.process_time()
-                lcs = alg.function(dna, dna_strand)
-                total_time = time.process_time() - start
-                results.append([alg.description, dna_size, dna_strand_size,
-                                i+1, total_time])
-
-                if verbose >= 2:
-                    print('  {:>30}: {:.3f}'.format(
-                        alg.description, total_time))
-                elif verbose >= 1:
+                _run_test(alg, dna, dna_size)
+                if verbose >= 1:
                     print('{}/{},'.format(i+1, repeat), end='', flush=True)
-
-                    # Make that the algorithm is working correctly
-                assert(lcs_utils.is_subsequence(dna, lcs))
 
     return results
 
 
-def memory(repeat=2, verbose=1):
+def _memory(repeat=2, verbose=1):
     '''Measures algorithms' memory usage.
 
     Memory usage has to be done separately from runtime measurement because
@@ -115,41 +124,109 @@ def memory(repeat=2, verbose=1):
               case (fast algorithms and/or small input size).
     '''
     results = []
+
+    # Run one test
+    def _run_test(alg, seq_size, subseq_size):
+        # Garbage-collec to not measure memory left over from other runs
+        gc.collect()
+
+        # Get the current memory usage of the Python environment
+        mem_baseline = min([min(memory_usage(
+            (lcs_empty.lcs, (dna, dna_strand)), interval=0.01))
+            for _ in range(3)])
+
+        start = time.process_time()
+        mem_usage = memory_usage((alg.function, (dna, dna_strand)),
+                                 interval=0.01)
+        total_time = time.process_time() - start
+        results.append([alg.description, dna_size, dna_strand_size,
+                        i + 1, max(mem_usage) - mem_baseline,
+                        total_time])
+        if verbose >= 2:
+            print('  {:>30}: {:.6f} {:.6f} {:.3f} {}'.format(
+                alg.description, max(mem_usage), mem_baseline, total_time,
+                mem_usage[:5]))
+
+    # Run all tests
     for dna_size, dna_strand_size in tests:
         # Create the test strings only once to correcly compare algorithms
         dna = lcs_utils.random_dna_sequence(dna_size)
         dna_strand = lcs_utils.random_dna_sequence(dna_strand_size)
 
-        if verbose >= 1:
-            print('\nMemory and time for DNA {:,}, DNA strand {:,}'.format(
-                len(dna), len(dna_strand)))
-
         for alg in algorithms:
             if verbose == 1:
                 print('\n{}: '.format(alg.description), end='', flush=True)
             for i in range(repeat):
-                # Run garbage collection so we don't measure memory left over
-                # from other algorithm runs
-                gc.collect()
-
-                # Get the current memory usage of the Python environment
-                mem_baseline = min([min(memory_usage(
-                    (lcs_empty.lcs, (dna, dna_strand)), interval=0.01))
-                    for _ in range(3)])
-
-                start = time.process_time()
-                mem_usage = memory_usage((alg.function, (dna, dna_strand)),
-                                         interval=0.01)
-                total_time = time.process_time() - start
-                results.append([alg.description, dna_size, dna_strand_size,
-                                i + 1, max(mem_usage) - mem_baseline,
-                                total_time])
-
-                if verbose >= 2:
-                    print('  {:>30}: {:.6f} {:.6f} {:.3f} {}'.format(
-                        alg.description, max(mem_usage), mem_baseline,
-                        total_time, mem_usage[:5]))
-                elif verbose >= 1:
+                _run_test(alg, dna, dna_strand)
+                if verbose >= 1:
                     print('{}/{},'.format(i+1, repeat), end='', flush=True)
 
     return results
+
+
+def runtime(repeat=2, verbose=1):
+    """Runs runtime tests and returns raw and summary statistics.
+
+    Keyword Arguments:
+        repeat {int} -- How many times to measure each algorithm.
+        verbose {int} -- Set to > 0 for different levels of outputs while
+            testing.
+
+    Returns:
+        DataFrame -- Raw results of all runs, as documented in the internal
+            function.
+        DataFrame -- Summary results, with average runtime for each experiment.
+    """
+    results_raw = _runtime(repeat, verbose)
+
+    # Raw results - all data points
+    results_raw_pd = pd.DataFrame(results_raw)
+    results_raw_pd.columns = [DF_ALGORITHM, DF_SEQ_SIZE, DF_SUBSEQ_SIZE,
+                              DF_TEST_NUMBER, DF_RUNTIME]
+
+    # Summary results - average runtime for each experiment
+    results_summary_pd = results_raw_pd.groupby(
+        [DF_ALGORITHM, DF_SEQ_SIZE, DF_SUBSEQ_SIZE]).mean()
+    # Average of test number doesn't make sense, so drop it
+    results_summary_pd.drop([DF_TEST_NUMBER], axis='columns', inplace=True)
+    # Flatten the results to make it easier to understand and use in graphs
+    results_summary_pd.reset_index(inplace=True)
+
+    return results_raw_pd, results_summary_pd
+
+
+def memory(repeat=2, verbose=1):
+    """Runs memory usage tests and returns raw and summary statistics.
+
+    Keyword Arguments:
+        repeat {int} -- How many times to measure each algorithm.
+        verbose {int} -- Set to > 0 for different levels of outputs while
+            testing.
+
+    Returns:
+        DataFrame -- Raw results of all runs, as documented in the internal
+            function.
+        DataFrame -- Summary results, with average memory usage for each
+            experiment.
+    """
+    results_raw = _memory(repeat, verbose)
+
+    # Raw results - all data points
+    results_raw_pd = pd.DataFrame(results_raw)
+    results_raw_pd.columns = [DF_ALGORITHM, DF_SEQ_SIZE, DF_SUBSEQ_SIZE,
+                              DF_TEST_NUMBER, DF_MEMORY, DF_RUNTIME]
+
+    # Despite all tricks to set a memory baseline, it still returns a baseline
+    # that is larger than the memory used by the algorithm. This results in
+    # negative memory usage in some cases. To prevent those results from
+    # affecting the average, they are ignored for the computations.
+    # Summary results - average runtime for each experiment
+    non_negative = results_raw_pd[results_raw_pd[DF_MEMORY] >= 0]
+    results_summary_pd = non_negative.groupby([DF_ALGORITHM, DF_SEQ_SIZE,
+                                               DF_SUBSEQ_SIZE]).mean()
+    # Average of test number doesn't make sense, so drop it
+    results_summary_pd.drop([DF_TEST_NUMBER], axis='columns', inplace=True)
+    # Flatten the results to make it easier to understand and use in graphs
+    results_summary_pd.reset_index(inplace=True)
+
+    return results_raw_pd, results_summary_pd
